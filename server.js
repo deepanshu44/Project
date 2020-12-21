b3a8df757ce2050d6bd5bbabd65688fd1fa188ab
deepanshu <deepanshuyadav.04@gmail.com>
2020-12-21 21:47:32 +0530

    working

--- server.js
+++ server.js
@@ -1,1 +1,85 @@
-console.log("hello");
+var express = require('express');
+require('dotenv').config();
+const app = express();
+var http = require('http').Server(app);
+var path=require('path');
+const multer = require('multer');
+var bodyParser = require('body-parser')
+var fs=require('fs')
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './src/component/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null , file.originalname);
+  }
+
+}
+)
+
+var upload = multer({storage:storage})
+
+app.use(express.static(path.join(__dirname, 'build')));
+//Import the mongoose module
+
+app.get('/', function (req, res) {
+  res.cookie('name', 'express').sendFile(path.join(__dirname, 'build', 'index.html'));
+
+});
+                
+app.get('/image',function(req,res){
+    let buff = fs.readFileSync(path.join(__dirname, 'src/', 'c-d-x-PDX_a_82obo-unsplash.jpg'),(err, data) => {
+                      if (err) throw err;
+                      console.log(data);
+                    });
+                    let base64data = buff.toString('base64');
+		res.send(base64data);
+              })
+                                
+
+var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+process.env.REACT_APP_PUBLIC_URL="hello"
+console.log("process",process.env.REACT_APP_PUBLIC_URL);
+
+  http.listen(8000,addresses[0], function() {  //,'192.168.1.18'
+    console.log('listening on'+addresses[0]+':8000');
+  });
--- src/Components/AppView/AppView.jsx
+++ src/Components/AppView/AppView.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import SimpleGrow from './../Grow/Grow.jsx'
+import React, { Suspense }  from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import axios from 'axios'
 import './AppView.scss'
 
-export default function AppView() {
-  const [timer,setTimer]=React.useState(false)
-  const [visible,setVisible]=React.useState('auto2-hidden')
-  setTimeout(()=>{ setVisible('auto2');setTimer(true)},2000)
+const OtherComponent = React.lazy(() =>import( './../Image/Image.jsx'))
+export default function AppView() {console.log("hello");
+  const [image,setImage]=React.useState('');
+  React.useEffect(()=>{
+    axios.get("http://192.168.1.18:8000/image").then((res)=>{
+       var base64Icon = 'data:image/png;base64,'+res.data;
+       setImage(base64Icon)
+       console.log("got res");
+    })
+  },[])
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="xl">
-        {timer?<SimpleGrow/>:''}
+
         <Grid container spacing={2}>
           <Grid item xs={6} md={12}>
             <TextField
@@ -30,17 +37,26 @@ export default function AppView() {
           alignItems="flex-start"
           spacing={2}
           >
-          <Grid item xs={12} className="test" >hello</Grid>
-          <Grid item xs={12} md={6} className="test" >
+          <Grid item xs={12} id='4' className="test">hello</Grid>
+          <Grid item xs={12} md={6} className="testdark">
           <Paper className="auto2-hidden">Test</Paper></Grid>
           <Grid item xs={6}className="test" >hello</Grid>
           <Grid item xs={7} className="test" >
+            <span style={{display:'flex',overflow:'hidden'}}><img src={image} /><img src={image} /></span>
           <Paper className="auto">xs=6</Paper>
         </Grid>
+        <Grid item md={12} >qqqqq</Grid>
           <Grid item xs className="auto" >hello</Grid>
+            <div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <OtherComponent />
+        </Suspense>
+      </div>
+<Grid item md={12} style={{backgroundColor:'white'}}><div><LinearProgress className='test' style={{width:'100%'}}/></div></Grid>
         </Grid>
 
       </Container>
+
 
     </React.Fragment>
   );
