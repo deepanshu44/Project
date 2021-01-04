a63ed269477d798f7f938a74d4103dc475624d82
deepanshu <deepanshuyadav.04@gmail.com>
2021-01-04 10:36:33 +0530

    deleted

--- src/Components/ProgressBar/ProgressBar.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Suspense }  from 'react';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import './ProgressBar.scss'
-
-export default function AppView() {
-  return (
-          <LinearProgress style={{width:'100%',height:'20px',marginTop:'20px'}}/>
-  );
-}
--- src/Components/index.jsx
+++
@@ -1,6 +0,0 @@
-import AppView from './AppView/AppView.jsx'
-import Container from '@material-ui/core/Container';
-
-export default function Component(){
-  return(<Container><AppView/></Container>)
-}
