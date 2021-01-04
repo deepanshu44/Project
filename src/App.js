e6447ca852547bda763c9e02029a9e11d459d7f0
deepanshu <deepanshuyadav.04@gmail.com>
2021-01-04 23:20:46 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import {View,NavigationBar} from './Components/index.js'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Provider,connect } from 'react-redux'
+import {createStore} from 'redux'
 
+const Reducer=(state=0,action)=>{
+  switch (action.type) {
+    case 'ADD':
+      return state+1;
+      case 'DEL':
+        return state-1;
+    default:return state;
+
+  }
+}
+//const Test=()=>{setTimeout(()=>{window.history.pushState('','','/123');window.history.go();},2000);return <div>refresh</div>;}
+let store=createStore(Reducer)
+// const actions={add:{type:"ADD"},del:{type:"DEL"}}
+// const mapDispatch=()=>actions
+// const mapState=(state)=>({value:state})
+// const ConnectedComp=connect(mapState,mapDispatch)(Test)
 function App() {
-  return (<><div>hello</div> </>
+
+  return (<Provider store={store}><CssBaseline/><NavigationBar/>
+    <div>helsslo</div>{console.log("value",store)}
+     <div>hello</div>
+      <div>helsslo</div><Test/>s
+       <View/></Provider>
   );
 }
 
---
+++ src/Components/View/View.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {Grid} from '@material-ui/core'
+export default function View(){
+  return <div>View</div>
+}
