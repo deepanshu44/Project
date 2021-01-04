7e1cd27882ff6095b7154092a1d11bc9989c67d0
deepanshu <deepanshuyadav.04@gmail.com>
2021-01-04 10:51:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import Component from './Components/index.jsx';
-import AppBar from './Components/AppBar/AppBar.jsx'
-import './scss/App.scss';
 
 function App() {
-  return (<><AppBar/><Component/></>
+  return (<><div>hello</div> </>
   );
 }
 
 export default App;
-
-// document.addEventListener('scroll',(e)=>{
-//   if(a.getBoundingClientRect().top===0){
-//     console.log("zero")};
-//     console.log(Math.trunc(Math.log(a.getBoundingClientRect().top)),
-//     Math.trunc(Math.log(a.getBoundingClientRect().bottom)))
-//   })
-
-//(a.getBoundingClientRect().y-window.pageYOffset)/window.pageYOffset
-
-// when element in viewport
-//a.getBoundingClientRect().y===window.innerHeight
-//near edge
